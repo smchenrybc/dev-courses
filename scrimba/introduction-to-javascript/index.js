94b8e2eeb09b6b17664a8a3ae8f8488932368732
Sean McHenry <sean@browndesign.com>
2018-10-23 12:56:47 -0400

    For Loop

--- scrimba/introduction-to-javascript/index.js
+++ scrimba/introduction-to-javascript/index.js
@@ -3,22 +3,27 @@
  */
 
 /*
-Switch
+For Loop
  */
 
-let studentAnswer = 'D';
+let total = 5;
 
-// switch is an if/else if statement
-switch(studentAnswer) {
-  case 'A':
-    console.log('A is wrong!');
-    break;
-  case 'B':
-    console.log('B is wrong!');
-    break;
-  case 'C':
-    console.log('C is correct!');
-    break;
-  default:
-    console.log('Please answer A to C!');
+// count to five!
+for (let x = 1; x <= total; x++) {
+  console.log(x);
 }
+
+let newTotal = 0;
+
+for (let x = 1; x <= total; x++) {
+  newTotal += x;
+}
+console.log(newTotal); // 1 + 2 + 3 + 4 + 5 = 15
+
+let numTotal = 0;
+let numArray = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < numArray.length; i++) {
+  numTotal += numArray[i];
+}
+console.log(numTotal);
