182814634e0a84894583edf419646793880c60d4
Sean McHenry <sean@browndesign.com>
2018-10-23 11:39:42 -0400

    Arithmetic Operators

--- scrimba/introduction-to-javascript/index.js
+++ scrimba/introduction-to-javascript/index.js
@@ -3,39 +3,24 @@
  */
 
 /*
-Objects
+Arithmetic Operators
  */
 
-let example1 = {
-  firstName: 'Sean',
-  lastName: 'McHenry',
-  address: {
-    city: 'Portsmouth',
-    state: 'NH'
-  },
-  age: 33,
-  cats: ['Sly', 'Louie']
-}
+// operators:
+// +, -, *, /
+
+let example1 = 5 + 5;
+
+// modulo operator is the
+// number of time a number divides
+// into the first number:
+
+// returns the whole number
+let example2 = 5 % 6;
+
+// returns 1
+let example3 = 5 % 4;
 
 console.log(example1);
-console.log(typeof(example1));
-
-
-console.log(`${example1.firstName} ${example1.lastName}`);
-
-console.log(`${example1.address.city}, ${example1.address.state}`);
-
-console.log(example1.cats);
-
-example1.cats.forEach(cat => {
-  console.log(cat);
-});
-
-// print the keys:
-console.log(Object.keys(example1));
-
-// print the values:
-console.log(Object.values(example1));
-
-// check for key
-console.log(example1.hasOwnProperty('firstName'));
+console.log(example2);
+console.log(example3);
