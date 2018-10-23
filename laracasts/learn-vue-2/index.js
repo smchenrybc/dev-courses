20ce7039e82ad9d09145d652a3f71747ab4b6916
Sean McHenry <sean@browndesign.com>
2018-10-23 16:06:14 -0400

    Setup Vue Devtools

---
+++ laracasts/learn-vue-2/index.js
@@ -0,0 +1,16 @@
+/**
+ * index.js
+ */
+
+// let data = {
+//   message: 'Hello, world!'
+// }
+
+// document.querySelector('#input').value = data.message;
+
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello, world!'
+  }
+});
