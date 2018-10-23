df8a6da571a5c51e943656568ff4b3c6331deb44
Sean McHenry <sean@browndesign.com>
2018-10-23 11:47:32 -0400

    Relational Operators

--- scrimba/introduction-to-javascript/index.js
+++ scrimba/introduction-to-javascript/index.js
@@ -3,24 +3,30 @@
  */
 
 /*
-Arithmetic Operators
+Relational Operators
  */
 
-// operators:
-// +, -, *, /
+// relational operators are
+// used for comparison and
+// return true or false.
 
-let example1 = 5 + 5;
+let example1 = 10;
+let example2 = 15;
 
-// modulo operator is the
-// number of time a number divides
-// into the first number:
+console.log(example1 >= example2); // false
 
-// returns the whole number
-let example2 = 5 % 6;
+// double equals versus single equals:
+// double equal checks that value and
+// type are equal, single just checks values
 
-// returns 1
-let example3 = 5 % 4;
+console.log(typeof example1); // number
+console.log(typeof example2); // number
 
-console.log(example1);
-console.log(example2);
-console.log(example3);
+example1 = 10; // number
+example2 = '10'; // string
+
+// are the two values equal?
+console.log(example1 == example2);
+
+// are values and types equal?
+console.log(example1 === example2);
