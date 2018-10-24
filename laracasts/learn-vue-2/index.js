021ba434ad60b91fe0feeb9eff46ad1ec00f14e5
Sean McHenry <sean@browndesign.com>
2018-10-24 14:44:20 -0400

    The Need for Computed Properties

--- laracasts/learn-vue-2/index.js
+++ laracasts/learn-vue-2/index.js
@@ -5,21 +5,41 @@
 var app = new Vue({
   el: "#root",
   data: {
-    titleClass: 'color-red',
-    buttonTitle: 'Button title set using Vue.js',
-    isLoading: false,
-    disabled: false
-  },
-  methods: {
-    method() {
-
-    },
-    toggleClass() {
-      if (this.isLoading === false) {
-        this.isLoading = true;
-      } else if (this.isLoading === true) {
-        this.isLoading = false;
+    message: 'Hello, world!',
+    tasks: [
+      {
+        description: 'Go to the store',
+        completed: true
+      },
+      {
+        description: 'Finish screencast',
+        completed: false
+      },
+      {
+        description: 'Clear inbox',
+        completed: false
+      },
+      {
+        description: 'Make dinner',
+        completed: false
+      },
+      {
+        description: 'Clean room',
+        completed: true
       }
+    ]
+  },
+  computed: {
+    reversedMessage() {
+      return this.message.split("").reverse().join("");
+    },
+    incompleteTasks() {
+      // this.tasks.filter(function(task) {
+      //   return !task.completed;
+      // });
+
+      // ES6 syntax:
+      return this.tasks.filter(task => !task.completed);
     }
   }
 });
