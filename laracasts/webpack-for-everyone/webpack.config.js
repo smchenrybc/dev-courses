23e4afea33b7bf72f793addedd2e621a599ebcbf
Sean McHenry <sean@browndesign.com>
2018-11-30 16:11:58 -0500

    Started "Webpack for Everyone" course

---
+++ laracasts/webpack-for-everyone/webpack.config.js
@@ -0,0 +1,14 @@
+/**
+ * webpack.config.js
+ */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
