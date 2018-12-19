09765812ab2f0b61c00564f1db574ca42e35efb9
Sean McHenry <sean@browndesign.com>
2018-12-19 15:53:47 -0500

    Finished episode 7

--- laracasts/webpack-for-everyone/src/main.js
+++ laracasts/webpack-for-everyone/src/main.js
@@ -2,21 +2,4 @@
  * main.js
  */
 
-require('./main.css');
-
-// class Form {
-//   constructor() {
-//     alert('Yay! Form classes are great no matter what JS developers say!');
-//   }
-// }
-
-class Form {
-  constructor() {
-    let num_array = [5, 10, 15];
-    let numbers = num_array.map(number => number * 2);
-
-    console.log(numbers);
-  }
-}
-
-new Form();
+require('./main.scss');
