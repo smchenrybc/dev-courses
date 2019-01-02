c12f02ddce87e6fc5f51f43a7803e5997c70f431
Sean McHenry <sean@browndesign.com>
2019-01-02 16:07:09 -0500

    Creating a simple bar chart

--- scrimba/learn-d3.js-for-free/index.js
+++ scrimba/learn-d3.js-for-free/index.js
@@ -3,14 +3,29 @@
  */
 
 // set var
-let dataset = [1, 2, 3, 4, 5];
+let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-d3.select('body')
-  .selectAll('p')
+let svgWidth = 500,
+  svgHeight = 300,
+  barPadding = 5;
+let barWidth = (svgWidth / dataset.length);
+
+let svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var barChart = svg.selectAll("rect")
   .data(dataset)
   .enter()
-  .append('p')
-  // .text('D3 is awesome!');
-  .text(function(d) {
+  .append("rect")
+  .attr("y", function(d) {
+    return svgHeight - d;
+  })
+  .attr("height", function(d) {
     return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function(d, i) {
+    let translate = [barWidth * i, 0];
+     return "translate("+ translate +")";
   });
