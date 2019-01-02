0c3d73f719226ec389c0fd201d1d21fe45f3d319
Sean McHenry <sean@browndesign.com>
2019-01-02 15:34:25 -0500

    Finished "Data Loading and Binding"

--- scrimba/learn-d3.js-for-free/index.js
+++ scrimba/learn-d3.js-for-free/index.js
@@ -3,23 +3,14 @@
  */
 
 // set var
-let select = d3.select('h1');
+let dataset = [1, 2, 3, 4, 5];
 
-// using multiple methods on our selection
-// so we're chaining the methods below:
-
-// set color
-select.style('color', 'red')
-
-// add class
-.attr('class', 'heading')
-
-// update text
-.text('Updated h1 text');
-
-let body = d3.select('body');
-body.append('p').text('First paragraph.');
-body.append('p').text('Second paragraph.');
-body.append('p').text('Third paragraph.');
-
-d3.selectAll('p').style('color', 'blue');
+d3.select('body')
+  .selectAll('p')
+  .data(dataset)
+  .enter()
+  .append('p')
+  // .text('D3 is awesome!');
+  .text(function(d) {
+    return d;
+  });
