e883015da0f69762099e1549f9d184c2bc74371a
Sean McHenry <sean@browndesign.com>
2019-01-03 13:44:18 -0500

    Creating labels

--- scrimba/learn-d3.js-for-free/index.js
+++ scrimba/learn-d3.js-for-free/index.js
@@ -6,15 +6,15 @@
 let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
 let svgWidth = 500,
-  svgHeight = 300,
-  barPadding = 5;
+    svgHeight = 300,
+    barPadding = 5;
 let barWidth = (svgWidth / dataset.length);
 
 let svg = d3.select('svg')
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var barChart = svg.selectAll("rect")
+let barChart = svg.selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
@@ -27,5 +27,21 @@ var barChart = svg.selectAll("rect")
   .attr("width", barWidth - barPadding)
   .attr("transform", function(d, i) {
     let translate = [barWidth * i, 0];
-     return "translate("+ translate +")";
+    //  return "translate("+ translate +")";
+    return `translate(${translate})`;
   });
+
+let text = svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d;
+  })
+  .attr("y", function(d, i) {
+    return svgHeight - d - 5;
+  })
+  .attr("x", function(d, i) {
+    return ( (barWidth * i) + 2);
+  })
+  .attr("fill", 'rebeccapurple');
