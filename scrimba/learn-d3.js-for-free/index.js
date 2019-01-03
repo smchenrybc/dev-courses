99830658a7d6a8758e09c117ab9e9a07fdae88d3
Sean McHenry <sean@browndesign.com>
2019-01-03 14:22:26 -0500

    Scales

--- scrimba/learn-d3.js-for-free/index.js
+++ scrimba/learn-d3.js-for-free/index.js
@@ -2,46 +2,40 @@
  * index.js
  */
 
-// set var
-let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// dataset
+let dataset = [1, 2, 3, 4, 5];
 
+// other vars
 let svgWidth = 500,
     svgHeight = 300,
     barPadding = 5;
 let barWidth = (svgWidth / dataset.length);
 
+// set SVG size:
 let svg = d3.select('svg')
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+// scale
+let yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgHeight - 5]);
+
+// bars
 let barChart = svg.selectAll("rect")
   .data(dataset)
   .enter()
   .append("rect")
   .attr("y", function(d) {
-    return svgHeight - d;
+    return svgHeight - yScale(d);
   })
   .attr("height", function(d) {
-    return d;
+    return yScale(d);
   })
   .attr("width", barWidth - barPadding)
   .attr("transform", function(d, i) {
     let translate = [barWidth * i, 0];
     //  return "translate("+ translate +")";
     return `translate(${translate})`;
-  });
-
-let text = svg.selectAll("text")
-  .data(dataset)
-  .enter()
-  .append("text")
-  .text(function(d) {
-    return d;
   })
-  .attr("y", function(d, i) {
-    return svgHeight - d - 5;
-  })
-  .attr("x", function(d, i) {
-    return ( (barWidth * i) + 2);
-  })
-  .attr("fill", 'rebeccapurple');
+  .attr("fill", "#333");
