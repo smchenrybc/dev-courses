59f9dddbf423b1cc593bda8391eff6b0c37cf677
Sean McHenry <sean@browndesign.com>
2019-01-07 15:01:50 -0500

    Finished "Create a line chart." Finished course.

--- scrimba/learn-d3.js-for-free/index.js
+++ scrimba/learn-d3.js-for-free/index.js
@@ -2,78 +2,106 @@
  * index.js
  */
 
-var data = [
-  {
-    platform: "Windows",
-    percentage: 37.43
-  },
-  {
-    platform: "Android",
-    percentage: 36.49
-  },
-  {
-    platform: "iOS",
-    percentage: 13.16
-  },
-  {
-    platform: "OSX",
-    percentage: 6.22
-  },
-  {
-    platform: "Unknown",
-    percentage: 4.01
-  },
-  {
-    platform: "Linux",
-    percentage: 0.85
-  }
-];
+// API endpoint
+const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
 
-var svgWidth = 500,
-  svgHeight = 300,
-  radius = Math.min(svgWidth, svgHeight) / 2;
-
-var svg = d3.select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// create group to hold pie chart
-var g = svg.append("g")
-  .attr("transform", `translate(${radius}, ${radius})`);
-
-var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-var pie = d3.pie().value(function(d) {
-  return d.percentage;
+/**
+ * Load data from API
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+  fetch(api)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let parsedData = parseData(data);
+      drawChart(parsedData);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
 });
 
-var path = d3.arc()
-  .outerRadius(radius)
-  .innerRadius(0);
+/**
+ * Parse data into key-value pairs
+ *
+ * @param {object} data object containing BPI historical data
+ */
+function parseData(data) {
+  let arr = [];
+  for (let i in data.bpi) {
+    arr.push({
+      date: new Date(i),
+      value: +data.bpi[i]
+    });
+  }
+  return arr;
+}
 
-var arc = g.selectAll("arc")
-  .data(pie(data))
-  .enter()
-  .append("g")
-  .attr("class", function(d) {
-    return d.data.platform.toLowerCase().replace(/\s/g, '');
-  });
+/**
+ * Create a chart using D3
+ */
+function drawChart(data) {
+  let svgWidth = 600,
+    svgHeight = 400;
+  let margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 50
+  };
+  let width = svgWidth - margin.left - margin.right;
+  let height = svgHeight - margin.top - margin.bottom;
 
-arc.append("path")
-  .attr("d", path)
-  .attr("fill", function(d) {
-    return color(d.data.percentage);
-  });
+  let svg = d3.select("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
 
-var label = d3.arc()
-  .outerRadius(radius)
-  .innerRadius(0)
+  let g = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-arc.append("text")
-  .attr("transform", function(d) {
-    return `translate(${label.centroid(d)})`;
-  })
-  .attr("text-anchor", "middle")
-  .text(function(d) {
-    return `${d.data.platform}: ${d.data.percentage}%`;
-  });
+  let x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+  let y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+  let line = d3.line()
+    .x(function(d) {
+      return x(d.date)
+    })
+    .y(function(d) {
+      return y(d.value)
+    })
+    x.domain(d3.extent(data, function(d) {
+      return d.date;
+    }));
+    y.domain(d3.extent(data, function(d) {
+      return d.value;
+    }));
+
+  g.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove();
+
+  g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Price ($)");
+
+  g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+}
