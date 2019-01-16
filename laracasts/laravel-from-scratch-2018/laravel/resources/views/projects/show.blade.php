8446bada106fcc5cd448ecf9c67ec8b593c9e9e7
Sean McHenry <sean@browndesign.com>
2019-01-16 13:58:21 -0500

    16: Your First Eloquent Relationships

--- laracasts/laravel-from-scratch-2018/laravel/resources/views/projects/show.blade.php
+++ laracasts/laravel-from-scratch-2018/laravel/resources/views/projects/show.blade.php
@@ -7,6 +7,16 @@
     {{ $project->description }}
   </div>
 
+  @if ($project->tasks->count())
+    <div>
+      <ul style="margin-bottom: 1.3em; list-style: circle; margin-left: 1.3em;">
+        @foreach ($project->tasks as $task)
+          <li>{{ $task->description }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
   <p>
     <a href="/projects/{{ $project->id }}/edit">Edit</a>
   </p>
