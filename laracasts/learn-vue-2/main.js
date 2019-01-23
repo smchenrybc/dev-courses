a75a5da45130d89eb204feba3ac9fc6b27ca12f4
Sean McHenry <sean@browndesign.com>
2019-01-23 16:49:29 -0500

    15: Single-Use Components and Inline Templates

--- laracasts/learn-vue-2/main.js
+++ laracasts/learn-vue-2/main.js
@@ -5,29 +5,12 @@
 /*
 Component
  */
-Vue.component("modal", {
-  template: `
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">
-            <slot name="header"></slot>
-          </p>
-          <button class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <slot></slot>
-        </section>
-        <footer class="modal-card-foot">
-          <slot name="footer">
-            <button class="button is-success">Save changes</button>
-            <button class="button">Cancel</button>
-          </slot>
-        </footer>
-      </div>
-    </div>
-  `
+Vue.component('progress-view', {
+  data() {
+    return {
+      completionRate: '50%'
+    }
+  }
 });
 
 /*
