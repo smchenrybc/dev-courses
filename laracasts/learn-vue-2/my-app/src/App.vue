0f07f07e4dca2ef14f12b53849692283a21480d4
Sean McHenry <sean@browndesign.com>
2019-01-24 10:40:48 -0500

    16: Webpack and vue-cli

---
+++ laracasts/learn-vue-2/my-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <message>Hello, world!</message>
+
+    <message>Hello again!</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Message
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  html {
+    font-size: 20px;
+  }
+  body {
+    padding: 30px;
+    font-family: sans-serif;
+    font-size: 1rem;
+    color: #333;
+  }
+  p {
+    margin: 0 0 1.3rem;
+  }
+</style>
