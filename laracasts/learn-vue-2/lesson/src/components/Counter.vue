1831ab48a482ca65e8881543a780bbf65e724af2
Sean McHenry <sean@browndesign.com>
2019-01-24 11:02:23 -0500

    17: Hot Module Replacement

---
+++ laracasts/learn-vue-2/lesson/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="mt0">The Count is: {{ count }}</h2>
+
+    <p>
+      <button class="btn btn-primary" @click="count += 5">Increment</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      };
+    }
+  }
+</script>
